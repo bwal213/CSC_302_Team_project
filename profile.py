e7253293280b2f277b4c8440c17a34e73ed12ad6
bwal213 <36938110+bwal213@users.noreply.github.com>
2018-11-10 17:13:30 -0500

    Added command for setup.sh.
    Commented out lines that were moved to setup.sh, and added a command to run it.

--- profile.py
+++ profile.py
@@ -19,12 +19,15 @@ node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU18-64-STD"
 node.routable_control_ip = "true"
 
 node.addService(rspec.Execute(shell="/bin/sh",
-                              command="sudo apt update"))
-node.addService(rspec.Execute(shell="/bin/sh",
-                              command="sudo apt install -y apache2"))
-node.addService(rspec.Execute(shell="/bin/sh",
-                              command='sudo ufw allow in "Apache Full"'))
-node.addService(rspec.Execute(shell="/bin/sh",
-                              command='sudo systemctl status apache2'))
+                              command="sudo bash /local/repository/setup_files/setup.sh"))
+
+#node.addService(rspec.Execute(shell="/bin/sh",
+#                              command="sudo apt update"))
+#node.addService(rspec.Execute(shell="/bin/sh",
+#                              command="sudo apt install -y apache2"))
+#node.addService(rspec.Execute(shell="/bin/sh",
+#                              command='sudo ufw allow in "Apache Full"'))
+#node.addService(rspec.Execute(shell="/bin/sh",
+#                              #command='sudo systemctl status apache2'))
 # Print the RSpec to the enclosing page.
 portal.context.printRequestRSpec()
