43a80a39d752df619b264c4c4625a71585e674e0
bwal213 <36938110+bwal213@users.noreply.github.com>
2018-11-14 18:33:51 -0500

    Change where paths are saved to system wide.

--- setup_files/setup.sh
+++ setup_files/setup.sh
@@ -40,9 +40,11 @@ sudo ufw enable
 #
 wget https://repo.anaconda.com/archive/Anaconda3-5.3.0-Linux-x86_64.sh
 sudo bash -c "bash Anaconda3-5.3.0-Linux-x86_64.sh -b -p /opt/anaconda3"
-sudo bash -c "echo 'ANACONDA_HOME=/opt/anaconda3/' >> /etc/profile"
-sudo bash -c "echo 'PATH=/opt/anaconda3/bin:$PATH' >> /etc/profile"
-sudo bash -c "echo 'unset XDG_RUNTIME_DIR' >> /etc/profile"
+#sudo bash -c "echo 'ANACONDA_HOME=/opt/anaconda3/' >> /etc/profile"
+sudo bash -c "echo 'ANACONDA_HOME=/opt/anaconda3/' >> /etc/environment"
+#sudo bash -c "echo 'PATH=/opt/anaconda3/bin:$PATH' >> /etc/profile"
+sudo bash -c "echo 'PATH=/opt/anaconda3/bin:$PATH' >> /etc/environment"
+#sudo bash -c "echo 'unset XDG_RUNTIME_DIR' >> /etc/profile"
 
 # create a user named seed with password dees. 
 sudo useradd -m -p WchOyJRR.1Qrc -s /bin/bash seed
