e848f5276ba9080de4603d74e89ae801ec28c130
bwal213 <36938110+bwal213@users.noreply.github.com>
2018-12-12 23:18:18 -0500

    Update setup.sh
    Added commands for replacing items with updated url / ip address, as well as finding it. Elgg seems to depend on the address a bit for what url to update to.

--- setup_files/setup.sh
+++ setup_files/setup.sh
@@ -96,8 +96,21 @@ sudo su seed -c 'git clone https://github.com/linhbngo/Computer-Security.git ~/C
 #
 sudo su seed -c "wget https://elgg.org/about/getelgg?forward=elgg-2.3.9.zip -O /home/seed/elgg-2.3.9.zip"
 
-sudo su seed -c "ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' -m1 >~/ip.txt"
+sudo su seed -c "testingip=$(ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' -m1); echo -n 'http://' > ~/ip.txt; echo -n $testingip> ~/ip.txt; echo -n '/CSRF/Elgg' >> ~/ip.txt"
 #mysql -uroot -pseedubuntu -e 'UPDATE 'elgg_csrf'.'elgg_sites_entity' SET 'url' = "http://test.myelgg.org/" WHERE guid = '1';'
+#sudo su seed -c "testingip=$(ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' -m1); echo -n '"'; echo -n $testingip> ~/ip.txt; echo -n '/CSRF/Elgg' >> ~/ip.txt"
+#sudo su seed -c "mysql -uroot -pseedubuntu -e 'LOAD DATA INFILE '~/ip.txt' INTO TABLE 'elgg_csrf'.'elff_sites_entity';"
+
+#echo -n $testingip > ~/ip.txt; echo -n '/CSRF/Elgg/' >> ~/ip.txt
+#sudo \cp ip.txt /var/lib/mysql-files/
+#mysql -uroot -pseedubuntu -e 'UPDATE 'elgg_csrf'.'elgg_sites_entity' SET url=LOAD_FILE("/var/lib/mysql-files/ip.txt") WHERE guid=1;'
+#mysql -uroot -pseedubuntu -e 'UPDATE 'elgg_csrf'.'elgg_csrfsites_entity' SET url=LOAD_FILE("/var/lib/mysql-files/ip.txt") WHERE guid=1;'
+
+#sudo grep -r "csrflabelgg" /var/
+#sed -i -- 's/http:\\\/\\\/www.csrflabelgg.com/130.127.135.3\\\/CSRF\\\/Elgg/g' *.js
+#sed -i -- 's/http:\/\/www.csrflabelgg.com/130.127.135.3\/CSRF\/Elgg/g' *.js
+#sed -i -- 's/http:\/\/www.csrflabelgg.com/130.127.135.3\/CSRF\/Elgg/g' *.css
+
 
 # set up anaconda
 sudo su seed -c "conda install -c anaconda beautifulsoup4"
