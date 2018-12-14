d462081069fec66511214ffea8d4a80910ad2d87
bwal213 <36938110+bwal213@users.noreply.github.com>
2018-12-13 23:41:17 -0500

    Update setup.sh
    Commented and polished for final version.

--- setup_files/setup.sh
+++ setup_files/setup.sh
@@ -1,39 +1,55 @@
-#this is where all the setup commands go
 #!/bin/bash
 set -x
 
 #
-# update Ubuntu's repository
+# Update Ubuntu's repository
 #
 sudo apt -y update
-#sudo apt -y upgrade                # This might not be needed
 
 #
-# setup apache2
+# Install & setup apache2
 #
 sudo apt install -y apache2
 sudo ufw allow in "Apache Full"
 sudo systemctl enable apache2
 
+#
+# Install mysql for Elgg
+#
 sudo apt install -y mysql-server
+
+#
+# Configure mysql
+# Make sure the database can be added.
+#
 sudo mysql -e"FLUSH PRIVILEGES;"
 sudo mysql -e"set password for 'root'@'localhost' ='seedubuntu';"
+
+#
+# Add the seed labs database
+#
 sudo mysql -uroot -pseedubuntu < /local/repository/setup_files/Seed_Databases.sql
+
+#
+# Configure mysql
+# Make sure the correct passwords are set and all permissions are good.
+#
 sudo mysql -e"FLUSH PRIVILEGES;"
 sudo mysql -e"set password for 'root'@'localhost' ='seedubuntu';"
 mysql -uroot -pseedubuntu "set password for 'phpmyadmin'@'localhost' ='seedubuntu';"
 #sudo mysqladmin -uroot password seedubuntu
+
+#
+# Install dependencies for Elgg
+#
 sudo apt install -y php libapache2-mod-php php-mysqlnd
 sudo env DEBIAN_FRONTEND=noninteractive apt -yq install phpmyadmin
 sudo apt install -y composer
 
 #
-# setup nmap
-#
-#sudo apt-get -y install nmap        # This is not needed
-
-#
-# add files from zips
+# Add files from seed labs
+# There is also the original zips they came from in that directory.
+# They were unpackaged so specific files could be edited, they are also a bit easier to copy this way.
 #
 sudo \cp -Rf /local/repository/setup_files/www /var/
 sudo \cp -Rf /local/repository/setup_files/elgg /var/
@@ -42,23 +58,35 @@ sudo \cp -Rf /local/repository/setup_files/php /etc/
 sudo \cp -Rf /local/repository/setup_files/phpmyadmin /etc/
 sudo \cp -Rf /local/repository/setup_files/mysql /etc/
 
-#sudo a2enmod rewrite
+#
+# Disable Apache2 mods
+# These were in seed labs but conflict with the new versions of things.
+#
 sudo a2dismod mpm_event
 sudo a2dismod php7.0
+
+#
+# Enable Apache2 mods
+# Need to enable the most recent version of php.
+#
 sudo a2enmod php7.2
+
+#
+# Restart Apache2 to apply changes.
+#
 sudo systemctl restart apache2
 
 #
-# open port 9090 and 9999 for all communications
+# Open port 8888 and 22 then enable the firewall
+# Used for jupyter and ssh respectivly
 #
-#sudo ufw allow 9090                 # I dont think this is needed either
-
 sudo ufw allow 8888
 sudo ufw allow 22
 sudo ufw --force enable
 
 #
-# setup Anaconda
+# Setup Anaconda
+# Legacy path commands are left incase needed in the future.
 #
 wget https://repo.anaconda.com/archive/Anaconda3-5.3.0-Linux-x86_64.sh
 sudo bash -c "bash Anaconda3-5.3.0-Linux-x86_64.sh -b -p /opt/anaconda3"
@@ -67,58 +95,102 @@ sudo bash -c "echo 'PATH="/opt/anaconda3/bin:/usr/local/sbin:/usr/local/bin:/usr
 #sudo bash -c "echo 'unset XDG_RUNTIME_DIR' >> /etc/profile"
 sudo bash -c "echo 'ANACONDA_HOME=/opt/anaconda3/' >> /etc/environment"
 #sudo bash -c "echo 'PATH=/opt/anaconda3/bin:$PATH' >> /etc/profile"
-
-# create a user named seed with password dees. 
-sudo useradd -m -p WchOyJRR.1Qrc -s /bin/bash seed
-
-# update root password
-# sudo usermod -p WcQ5Q3no8GLAk root
-sudo usermod -p WcQ5Q3no8GLAk root
-
-# add seed to sudo
-sudo usermod -aG sudo seed
-sudo usermod -aG root seed
-sudo su seed -c "touch ~/.sudo_as_admin_successful"
 #sudo su seed -p -c "source /etc/profile"
-sudo su seed -c "cd ~/ && unset XDG_RUNTIME_DIR && nohup jupyter notebook --NotebookApp.token='' --ip * --no-browser > ~/nohup_jupyter.out &"
-#sudo su seed -p -c "unset ETC_RUNTIME_DIR && source /etc/profile && jupyter notebook --NotebookApp.token='' --ip * --no-browser"
 
 #
-# setup git and gdb-peda
+# Create a user named seed with password dees. 
+#
+sudo useradd -m -p WchOyJRR.1Qrc -s /bin/bash seed
+
+#
+# Update root password
+# 
+sudo usermod -p WcQ5Q3no8GLAk root
+
+#
+# Add seed to sudo and root for jupyter
+#
+sudo usermod -aG sudo seed
+sudo usermod -aG root seed
+
+#
+# Touch the file so that sudo message goes away.
+#
+sudo su seed -c "touch ~/.sudo_as_admin_successful"
+
+#
+# Start jupyter notebook as seed
+# This uses nohup, and outputs to a file in the /home/seed directory.
+# Not configured for a error file.
+#
+sudo su seed -c "cd ~/ && unset XDG_RUNTIME_DIR && nohup jupyter notebook --NotebookApp.token='' --ip * --no-browser > ~/nohup_jupyter.out &"
+
+#
+# Install git and gdb-peda
 #
 sudo apt install git -y
 sudo su seed -c 'git clone https://github.com/longld/peda.git ~/peda'
 sudo su seed -c 'echo "source ~/peda/peda.py" >> ~/.gdbinit'
+
+#
+# Add the class repository so it can be seen from jupyter
+#
 sudo su seed -c 'git clone https://github.com/linhbngo/Computer-Security.git ~/Computer-Security/'
 
 #
-# get new elgg
+# Get new elgg then unzip
 #
-sudo su seed -c "wget https://elgg.org/about/getelgg?forward=elgg-2.3.9.zip -O /home/seed/elgg-2.3.9.zip"
-sudo su seed -c "unzip elgg-2.3.9.zip"
+sudo su seed -c "wget https://elgg.org/about/getelgg?forward=elgg-2.3.9.zip -O /home/seed/setup/elgg-2.3.9.zip"
+sudo su seed -c "cd ~/setup && unzip elgg-2.3.9.zip"
+
+#
+# Overwrite old elgg installs from seed lab
+# This needs to be done, as there are errors that are fixed in the latest version.
+# The old version seemed to be incompatible with php7.2.
+#
 sudo \cp -Rf /home/seed/elgg-2.3.9/* /var/www/CSRF/Elgg/
 sudo \cp -Rf /home/seed/elgg-2.3.9/* /var/www/XSS/Elgg/
 
-#sudo su seed -c "ipaddr=$(ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' -m1); sudo echo 'IPADDR=ipaddr'>> /etc/environment"
+#
+# Get current public IP and make it into a system-wide system variable
+#
 sudo su root -c "echo IPADDR=$(ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' -m1) >> /etc/environment"
-sudo su seed -c "echo -n 'http://' > ~/CSRFurl.txt; echo -n $IPADDR >> ~/CSRFurl.txt; echo -n '/CSRF/Elgg/' >> ~/CSRFurl.txt"
-sudo su seed -c "echo -n 'http://' > ~/XSSurl.txt; echo -n $IPADDR >> ~/XSSurl.txt; echo -n '/XSS/Elgg/' >> ~/XSSurl.txt"
 
-#mysql -uroot -pseedubuntu -e 'UPDATE 'elgg_csrf'.'elgg_sites_entity' SET 'url' = "http://test.myelgg.org/" WHERE guid = '1';'
-#sudo su seed -c "testingip=$(ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' -m1); echo -n '"'; echo -n $testingip> ~/ip.txt; echo -n '/CSRF/Elgg' >> ~/ip.txt"
-#sudo su seed -c "mysql -uroot -pseedubuntu -e 'LOAD DATA INFILE '~/ip.txt' INTO TABLE 'elgg_csrf'.'elff_sites_entity';"
+#
+# Create text files that contain the URL of the web pages.
+# This will be used to input them into the database.
+#
+sudo su seed -c "echo -n 'http://' > ~/setup/CSRFurl.txt; echo -n $IPADDR >> ~/setup/CSRFurl.txt; echo -n '/CSRF/Elgg/' >> ~/setup/CSRFurl.txt"
+sudo su seed -c "echo -n 'http://' > ~/setup/XSSurl.txt; echo -n $IPADDR >> ~/setup/XSSurl.txt; echo -n '/XSS/Elgg/' >> ~/setup/XSSurl.txt"
 
-#echo -n $testingip > ~/ip.txt; echo -n '/CSRF/Elgg/' >> ~/ip.txt
+#
+# These are legacy commands that helped to establish how to use thier seperate parts 
+#
+##sudo su seed -c "testingip=$(ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' -m1); echo -n '"'; echo -n $testingip> ~/ip.txt; echo -n '/CSRF/Elgg' >> ~/ip.txt"
+##echo -n $testingip > ~/ip.txt; echo -n '/CSRF/Elgg/' >> ~/ip.txt
 
-sudo \cp CSRFurl.txt /var/lib/mysql-files/
-sudo \cp XSSurl.txt /var/lib/mysql-files/
+#
+# Copy the text files to where mysql can see them
+#
+sudo \cp /home/seed/setup/CSRFurl.txt /var/lib/mysql-files/
+sudo \cp /home/seed/setup/XSSurl.txt /var/lib/mysql-files/
+
+#
+# Load the data in the files into the database to replace the old site URLs
+#
 mysql -uroot -pseedubuntu -e 'UPDATE 'elgg_csrf'.'elgg_sites_entity' SET url=LOAD_FILE("/var/lib/mysql-files/CSRFurl.txt") WHERE guid=1;'
 mysql -uroot -pseedubuntu -e 'UPDATE 'elgg_csrf'.'elgg_csrfsites_entity' SET url=LOAD_FILE("/var/lib/mysql-files/CSRFurl.txt") WHERE guid=1;'
 mysql -uroot -pseedubuntu -e 'UPDATE 'elgg_xss'.'elgg_xsssites_entity' SET url=LOAD_FILE("/var/lib/mysql-files/XSSurl.txt") WHERE guid=1;'
 
+#
+# These commands are very useful for debugging and finding what to replace
+#
 #sudo grep -r "csrflabelgg" /var/
-#cat ip.txt | head -c-1
+#sudo grep -r "xsslabelgg" /var/
 
+#
+# Replace the old URL with the new one in the cache files
+#
 sudo sed -i -- "s@http:\/\/www.csrflabelgg.com@http:\/\/$IPADDR\/CSRF\/Elgg@g" /var/elgg/csrf/views_simplecache/1501099611/default/*.js
 sudo sed -i -- "s@http://www.csrflabelgg.com@http://$IPADDR/CSRF/Elgg@g" /var/elgg/csrf/views_simplecache/1501099611/default/elgg/*.js
 sudo sed -i -- "s@http://www.csrflabelgg.com@http://$IPADDR/CSRF/Elgg@g" /var/elgg/csrf/views_simplecache/1501099611/default/*.css
@@ -126,12 +198,30 @@ sudo sed -i -- "s@http:\/\/www.xsslabelgg.com@http:\/\/$IPADDR\/XSS\/Elgg@g" /va
 sudo sed -i -- "s@http://www.xsslabelgg.com@http://$IPADDR/XSS/Elgg@g" /var/elgg/xss/views_simplecache/1501099743/default/elgg/*.js
 sudo sed -i -- "s@http://www.xsslabelgg.com@http://$IPADDR/XSS/Elgg@g" /var/elgg/xss/views_simplecache/1501099743/default/*.css
 
+#
+# Set the permissions so elgg can modify them
+# Yes, 777 is excessive, but I wanted to be sure.
+#
 sudo chmod -R 777 /var/elgg
 sudo chmod -R 777 /var/www
 
+#
+# Run the files to have elgg recognize the new version and change things accordingly
+# After this command the sites should be good to go, and able to be logged into.
+#
 sudo su seed -c 'php /var/www/CSRF/Elgg/upgrade.php'
 sudo su seed -c 'php /var/www/XSS/Elgg/upgrade.php'
 
-# set up anaconda
+#
+# Needed for a finished product. ;)
+# Thanks for a great semester Dr. Ngo!
+# This was one heck of a challenge.
+#
+sudo apt -y install sl
+
+#
+# Set up anaconda
+# This might not be needed, but helps so the students don't have to sit around so long at the start of class
+#
 sudo su seed -c "conda install -c anaconda beautifulsoup4"
 sudo su seed -c "conda install -c anaconda requests"
